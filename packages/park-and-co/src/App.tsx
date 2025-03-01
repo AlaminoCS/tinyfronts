332f74ecc5985e0c364c3d708c5ab4b3ac0c1ffc
Vanderlei José Alamino <valamino@mtrix.com.br>
2025-03-01 13:47:40 -0300

    chore: apply Prettier formatting across the monorepo

--- packages/park-and-co/src/App.tsx
+++ packages/park-and-co/src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from 'react'
 
 const App: React.FC = () => (
   <div>
     <h1>Bem-vindo ao Park&Co Estacionamentos!</h1>
     <p>Este é um microfrontend dedicado ao cliente Park&Co.</p>
   </div>
-);
+)
 
-export default App;
+export default App

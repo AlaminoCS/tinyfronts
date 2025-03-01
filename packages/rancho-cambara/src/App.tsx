f44397d5de6f56414c4fdcf4940b187044bd06fe
Vanderlei José Alamino <valamino@mtrix.com.br>
2025-03-01 12:48:48 -0300

    feat: add rancho-cambara microfrontend with React and TypeScript

---
+++ packages/rancho-cambara/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App: React.FC = () => (
+  <div>
+    <h1>Bem-vindo ao Rancho Cambará!</h1>
+    <p>Este é um microfrontend dedicado ao cliente Rancho Cambará.</p>
+  </div>
+);
+
+export default App;

928c9cf313a22091015243176f2720f0777e8ac9
Vanderlei José Alamino <valamino@mtrix.com.br>
2025-03-04 19:42:06 -0300

    teste

--- packages/pizza-party/src/App.tsx
+++ packages/pizza-party/src/App.tsx
@@ -4,8 +4,6 @@ const App: React.FC = () => (
   <div>
     <h1>Welcome to Pizza Party!</h1>
     <p>This is a microfrontend example with React and TypeScript.</p>
-    <p>I'm build a project using CI/CD right now.</p>
-    <p>I'm testing using deploy</p>
   </div>
 )
 

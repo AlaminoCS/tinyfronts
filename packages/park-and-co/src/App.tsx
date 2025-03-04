21be7a90143cffd081271b275928377c5f3ff6f4
Vanderlei José Alamino <valamino@mtrix.com.br>
2025-03-04 18:00:07 -0300

    feat: update pizza-party

--- packages/park-and-co/src/App.tsx
+++ packages/park-and-co/src/App.tsx
@@ -4,6 +4,7 @@ const App: React.FC = () => (
   <div>
     <h1>Bem-vindo ao Park&Co Estacionamentos!</h1>
     <p>Este é um microfrontend dedicado ao cliente Park&Co.</p>
+    <p>Vou tentar conectar ao Vercel novamente.</p>
   </div>
 )
 

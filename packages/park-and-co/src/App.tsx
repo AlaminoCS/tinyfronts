7ad33f11058d5b39acd24d61d799f8a990090736
Vanderlei José Alamino <valamino@mtrix.com.br>
2025-03-04 18:45:57 -0300

    feat: test

--- packages/park-and-co/src/App.tsx
+++ packages/park-and-co/src/App.tsx
@@ -5,7 +5,6 @@ const App: React.FC = () => (
     <h1>Bem-vindo ao Park&Co Estacionamentos!</h1>
     <p>Este é um microfrontend dedicado ao cliente Park&Co.</p>
     <p>Este é um novo teste</p>
-    <p>Este é um novo teste</p>
   </div>
 )
 

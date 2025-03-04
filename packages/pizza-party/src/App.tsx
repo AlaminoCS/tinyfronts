f235106fdf9188ef55780faf39fb32f71b7ccf97
Vanderlei José Alamino <valamino@mtrix.com.br>
2025-03-04 18:48:37 -0300

    test: novo teste de deploy

--- packages/pizza-party/src/App.tsx
+++ packages/pizza-party/src/App.tsx
@@ -5,7 +5,6 @@ const App: React.FC = () => (
     <h1>Welcome to Pizza Party!</h1>
     <p>This is a microfrontend example with React and TypeScript.</p>
     <p>I'm build a project using CI/CD right now.</p>
-    <p>This is a new test</p>
   </div>
 )
 
--- packages/rancho-cambara/src/App.tsx
+++ packages/rancho-cambara/src/App.tsx
@@ -4,7 +4,7 @@ const App: React.FC = () => (
   <div>
     <h1>Bem-vindo ao Rancho Cambará!</h1>
     <p>Este é um microfrontend dedicado ao cliente Rancho Cambará.</p>
-    <p>Vamos fazer um deploy</p>
+    <p>Vamos fazer um deploy automaticamente</p>
   </div>
 )
 

735e914122a9b4ddc629b2deaa478b7ee0885b11
Vanderlei José Alamino <valamino@mtrix.com.br>
2025-03-05 18:51:03 -0300

    install cargo-component

--- packages/cargo-components/src/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as Button } from './Button'
-export { lightTheme, darkTheme } from './theme'
-export { default as GlobalStyles } from './styles/GlobalStyles'
--- packages/pizza-party/src/App.tsx
+++ packages/pizza-party/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyles } from 'cargo-components'
-import Count from './components/Count'
 import { pizzaPartyTheme } from './theme'
+import Count from './components/Count'
 
 const App: React.FC = () => {
   return (

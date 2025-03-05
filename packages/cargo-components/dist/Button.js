d26e886bce822bdb116802d8ac0e8409b58167fa
Vanderlei José Alamino <valamino@mtrix.com.br>
2025-03-05 14:28:21 -0300

    Remove dist folder from version control

--- packages/cargo-components/dist/Button.js
+++
@@ -1,6 +0,0 @@
-import { jsxs as _jsxs } from 'react/jsx-runtime'
-
-const Button = ({ label, onClick }) => {
-  return _jsxs('button', {onClick, children: [label, ' +++']})
-}
-export default Button
--- packages/cargo-components/dist/styles/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-export const Button = styled.button `
-    padding: 10px 20px;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    background-color: #007bff;
-    color: #fff;
-`;

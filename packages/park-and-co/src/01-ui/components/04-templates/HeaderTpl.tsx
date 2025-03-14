9b8c2d9d62d32e0013e62a907f849603412c998e
Vanderlei José Alamino <valamino@mtrix.com.br>
2025-03-13 23:07:07 -0300

    feat: implementar context para navegação

--- packages/park-and-co/src/01-ui/components/04-templates/HeaderTpl.tsx
+++ packages/park-and-co/src/01-ui/components/04-templates/HeaderTpl.tsx
@@ -1,12 +1,64 @@
 import BigImgCarousel from '../03-organisms/BigImgCarousel'
-import TopNavOrg from '../03-organisms/TopNavOrg'
+import EmailIcon from '@mui/icons-material/Email'
+import PhoneIcon from '@mui/icons-material/Phone'
+import LocationOnIcon from '@mui/icons-material/LocationOn'
+import { TopNavProvider, TopNavOrg } from 'cargo-components'
 
 const HeaderTpl = () => {
+  const IMAGES = {
+    logo: new URL('../../../04-assets/images/logo.png', import.meta.url).href,
+  }
+
+  const contactLinks = [
+    {
+      text: 'contato@parkandco.com',
+      href: 'mailto:contato@parkandco.com',
+      icon: <EmailIcon />,
+    },
+    {
+      text: '+55 11 98765-4321',
+      href: 'tel:+5511987654321',
+      icon: <PhoneIcon />,
+    },
+    {
+      text: 'Rua dos Parques, 789',
+      href: 'https://maps.google.com/?q=R.+dos+Parques,+789',
+      icon: <LocationOnIcon />,
+    },
+  ]
+
+  const mainLinks = [
+    {
+      text: 'Quem somos',
+      href: '#quem-somos',
+    },
+    {
+      text: 'Nossos diferenciais',
+      href: '#nossos-diferenciais',
+    },
+    {
+      text: 'Serviços',
+      href: '#servicos',
+    },
+    {
+      text: 'Contato',
+      href: '#contato',
+    },
+  ]
+
   return (
-    <header>
-      <TopNavOrg />
-      <BigImgCarousel />
-    </header>
+    <TopNavProvider
+      value={{
+        contactLinks,
+        mainLinks,
+        logoSrc: IMAGES.logo,
+      }}
+    >
+      <header>
+        <TopNavOrg />
+        <BigImgCarousel />
+      </header>
+    </TopNavProvider>
   )
 }
 

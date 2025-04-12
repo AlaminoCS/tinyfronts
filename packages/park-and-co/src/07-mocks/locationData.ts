de32ae2512d53601e51298b053594c396319d492
Vanderlei José Alamino <valamino@mtrix.com.br>
2025-04-12 13:47:21 -0300

    corrigir endereços

--- packages/park-and-co/src/07-mocks/locationData.ts
+++ packages/park-and-co/src/07-mocks/locationData.ts
@@ -2,29 +2,61 @@ export const locationData = {
   title: 'Onde estamos',
   locations: [
     {
-      title: 'Sorocaba - Matriz',
-      text: 'Avenida Comendador Pereira Inácio, 900 - Jardim Vergueiro, Sorocaba/SP',
+      title: 'Sorocaba - unidade 1',
+      text: 'Avenida Comendador Pereira Inácio, 900 - Jardim Vergueiro, Sorocaba - SP',
       map: {
         title: 'Localização no Google Maps - Sorocaba',
-        src: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.5213618891225!2d-47.458225425423954!3d-23.513743559843032!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c58a84ebf90e83%3A0xaaba7d54565fad40!2sAv.%20Comendador%20Pereira%20In%C3%A1cio%2C%20900%20-%20Jardim%20Vergueiro%2C%20Sorocaba%20-%20SP%2C%2018030-005!5e0!3m2!1spt-BR!2sbr!4v1741374513958!5m2!1spt-BR!2sbr',
-      },
+        src: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.713979883578!2d-47.4597964!3d-23.5068095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c58a976d71385f%3A0xe73660b42f3de185!2sCondom%C3%ADnio%20Edif%C3%ADcio%20Henrique%20Cattani%20-%20R.%20Pandi%C3%A1%20Cal%C3%B3geras%2C%20418%20-%20Jardim%20Vergueiro%2C%20Sorocaba%20-%20SP%2C%2018030-030!5e0!3m2!1spt-BR!2sbr!4v1743702109837!5m2!1spt-BR!2sbr',
+      },        
     },
     {
-      title: 'São Paulo - Filial',
-      text: 'Avenida Paulista, 1000 - Bela Vista, São Paulo/SP',
+      title: 'Sorocaba - unidade 2',
+      text: 'R. Pandiá Calógeras, 418 - Jardim Vergueiro, Sorocaba - SP',
       map: {
         title: 'Localização no Google Maps - São Paulo',
         src: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.098839460559!2d-46.65439708502168!3d-23.564616184685214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59c8da0aa315%3A0x63b9f0e3703c751a!2sAv.%20Paulista%2C%201000%20-%20Bela%20Vista%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2001310-100!5e0!3m2!1spt-BR!2sbr!4v1629900000000!5m2!1spt-BR!2sbr',
       },
     },
     {
-      title: 'Campinas - Filial',
-      text: 'Avenida Francisco Glicério, 500 - Centro, Campinas/SP',
+      title: 'Sorocaba - unidade 3',
+      text: 'Avenida Comendador Pereira Inácio, 950 - Jardim Vergueiro, Sorocaba - SP',
       map: {
-        title: 'Localização no Google Maps - Campinas',
-        src: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.356219020248!2d-47.06277708503589!3d-22.902899385015085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8c8f6a9d99625%3A0x3c7498df0b6f4247!2sAv.%20Francisco%20Glic%C3%A9rio%2C%20500%20-%20Centro%2C%20Campinas%20-%20SP%2C%2013012-000!5e0!3m2!1spt-BR!2sbr!4v1629900000000!5m2!1spt-BR!2sbr',
+        title: 'Localização no Google Maps - Sorocaba',
+        src: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.5052773584484!2d-47.4555794!3d-23.514322500000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c58b9fc01e52ab%3A0x8562689a1dbf2f6a!2sAv.%20Comendador%20Pereira%20In%C3%A1cio%2C%20950%20-%20Jardim%20Vergueiro%2C%20Sorocaba%20-%20SP%2C%2018030-005!5e0!3m2!1spt-BR!2sbr!4v1743702367056!5m2!1spt-BR!2sbr',
       },
     },
+    {
+      title: 'Sorocaba - unidade 4',
+      text: 'Rua Padre Luiz, 269 - Centro Sorocaba - SP',
+      map: {
+        title: 'Localização no Google Maps - Sorocaba',
+        src: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.9219197891707!2d-47.4602844!3d-23.499321699999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c58abc9e4129d1%3A0xf7d0316b52fb9b49!2sR.%20Padre%20Luiz%2C%20269%20-%20Centro%2C%20Sorocaba%20-%20SP%2C%2018035-011!5e0!3m2!1spt-BR!2sbr!4v1743702428204!5m2!1spt-BR!2sbr',
+      },
+    },
+    {
+      title: 'Sorocaba - unidade 5',
+      text: 'Rua Imperatriz Leopoldina, 60 - Cerrado, Sorocaba - SP',
+      map: {
+        title: 'Localização no Google Maps - Sorocaba',
+        src: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.762275566604!2d-47.4720629!3d-23.5050706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c58ac10ddd4ee5%3A0xe231511152a9c30f!2sR.%20Imperatriz%20Leopoldina%2C%2060%20-%20Cerrado%2C%20Sorocaba%20-%20SP%2C%2018044-010!5e0!3m2!1spt-BR!2sbr!4v1743702473032!5m2!1spt-BR!2sbr',
+      },
+    },
+    {
+      title: 'Sorocaba - unidade 6',
+      text: 'Rua Paulo Antonio do Nascimento, 145 - Parque Campolim, Sorocaba - SP',
+      map: {
+        title: 'Localização no Google Maps - Sorocaba',
+        src: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.3536459379866!2d-47.4625013!3d-23.519779600000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c58a8939bb2d6d%3A0x8ed009e8218d9023!2sR.%20Paulo%20Ant%C3%B4nio%20do%20Nascimento%2C%20145%20-%20Parque%20Campolim%2C%20Sorocaba%20-%20SP%2C%2018047-400!5e0!3m2!1spt-BR!2sbr!4v1743702527179!5m2!1spt-BR!2sbr',          
+      },
+    },
+    {
+      title: 'Sorocaba - unidade 7',
+      text: 'Rua 28 de outubro, 120 - Jardim do Paço, Sorocaba - SP',
+      map: {
+        title: 'Localização no Google Maps - Sorocaba',
+        src: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3659.624428221035!2d-47.42148039999999!3d-23.4740081!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf6077a88325bd%3A0xa9bd63414ef7f60b!2sR.%20Vinte%20e%20Oito%20de%20Outubro%2C%20120%20-%20Jardim%20do%20Pa%C3%A7o%2C%20Sorocaba%20-%20SP%2C%2018087-080!5e0!3m2!1spt-BR!2sbr!4v1743702576275!5m2!1spt-BR!2sbr',         
+      },
+    }
   ],
   id: 'localizacao',
-}; 
+};
